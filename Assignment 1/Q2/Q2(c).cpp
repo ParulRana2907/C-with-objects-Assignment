c2f583de73a97b6c6a76f1a1109aa67da744196b
ParulRana2907 <parulrana2407@gmail.com>
2025-02-08 13:14:28 +0530

    Q2(c).cpp
    .cpp

--- Assignment 1/Q2/Q2(c).cpp
+++ Assignment 1/Q2/Q2(c).cpp
@@ -1,1 +1,27 @@
+//display the second largest and second smallest elements in the array
+#include <iostream>
+using namespace std;
 
+int main() {
+    int a;
+    cout << "Enter size: ";
+    cin >> a;
+
+    int arr[a];
+    cout << "Enter " << a << " elements: ";
+    for (int i = 0; i < a; i++) cin >> arr[i];
+
+    int max1 = arr[0], max2 = arr[0];
+    int min1 = arr[0], min2 = arr[0];
+
+    for (int i = 1; i < a; i++) {
+        if (arr[i] > max1) max2 = max1, max1 = arr[i];
+        else if (arr[i] > max2 && arr[i] != max1) max2 = arr[i];
+
+        if (arr[i] < min1) min2 = min1, min1 = arr[i];
+        else if (arr[i] < min2 && arr[i] != min1) min2 = arr[i];
+    }
+
+    cout << "Second Largest: " << max2 << "\nSecond Smallest: " << min2;
+    return 0;
+}
