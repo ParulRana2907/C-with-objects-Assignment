005c7e8e275b05de58b91be1c9f75713803e6260
Parul Rana <parulrana2407@gmail.com>
2025-02-08 19:22:29 +0530

    Q4.cpp
    .cpp

--- Assignment 1/Q4/Q4.cpp
+++ Assignment 1/Q4/Q4.cpp
@@ -1,23 +1,40 @@
 // print a spiral pattern of numbers for a given size n
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main() {
-    int n;
-    cout << "Enter the value  of n for matrix: ";
-    cin >> n;
-    int matrix[n][n];
-    int number = 1;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            matrix[i][j] = number++;
-        }
+int main(){
+    int size;
+    cout<<"Enter size: ";
+    cin>>size;
+    int matrix[size][size];
+    int number=1,top=0,bottom=size-1,left=0,right=size-1;
+
+while(number<=size*size){
+    for(int i=left;i<=right;i++)
+    matrix[top][i]=number++;
+    top++;
+
+for(int i=top;i<=bottom;i++)
+matrix[i][right]=number++;
+right--;
+
+if(top<=bottom)
+for(int i=right;i>=left;i--)
+matrix[bottom][i]=number++;
+bottom--;
+
+if(left<=right)
+for(int i=bottom;i>=top;i--)
+matrix[i][left]=number++;
+left++;
+
+}
+cout<<"Spiral Matrix: "<<endl;
+for(int i=0;i<size;i++){
+    for(int j=0;j<size;j++)
+    cout<<matrix[i][j]<<" ";
+    cout<<endl;
     }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
+    
+return 0;
     }
 
-    return 0;
-}
