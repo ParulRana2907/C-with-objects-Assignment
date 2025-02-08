1eccf22f9c949b115216631106f53994fee5e8c4
ParulRana2907 <parulrana2407@gmail.com>
2025-02-08 13:22:13 +0530

    Q3(b).cpp
    .cpp

--- Assignment 1/Q3/Q3(b).cpp
+++ Assignment 1/Q3/Q3(b).cpp
@@ -1,1 +1,20 @@
+// check whether the string is a palindrome (ignoring spaces and case sensitivity)
+#include <iostream>
+using namespace std;
 
+int main() {
+    string str, rev = "";
+    cout << "Enter a string: ";
+    cin >> str;
+
+    for (int i = str.length() - 1; i >= 0; i--) {
+        rev += str[i];
+    }
+
+    if (str == rev)
+        cout << "The given string is Palindrome\n";
+    else
+        cout << "The given string is not a palindrome\n";
+
+    return 0;
+}
