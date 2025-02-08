b291fae4418734d0b4d2f66568108f4e24ba692c
Parul Rana <parulrana2407@gmail.com>
2025-02-08 19:15:04 +0530

    Q3(d).cpp
    .cpp

--- Assignment 1/Q3/Q3(d).cpp
+++ Assignment 1/Q3/Q3(d).cpp
@@ -1,18 +1,1 @@
-// replace all vowels in the string with a specific character (eg: *)
-#include <iostream>
-#include <string>
-using namespace std;
 
-int main() {
-    string input;
-    cout << "Please enter a string: ";
-    getline(cin, input);
-    for (char &c : input) {
-        c = tolower(c);
-        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-            c = '*';
-        }
-    }
-    cout << "Modified string: " << input << endl;
-    return 0;
-}
