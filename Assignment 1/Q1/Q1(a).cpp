27355afd566291890be4204c8faa992c0a711abd
ParulRana2907 <parulrana2407@gmail.com>
2025-02-08 12:58:42 +0530

    Q1(a).cpp
    .cpp

--- Assignment 1/Q1/Q1(a).cpp
+++ Assignment 1/Q1/Q1(a).cpp
@@ -1,1 +1,18 @@
+// check whether n is a prime number
+#include <iostream>
+using namespace std;
+int main(){
+int num;
+  cout<<"enter the number:";
+  cin>>num;
+  for (int i=2;i<num;i++){
+     if (num%i==0){
+      cout<<"it is not a  prime number"<<endl;
+      return 0;
+  }
+ }
+cout<< "it is a prime number";       
+
+  return 0;
+}
 
