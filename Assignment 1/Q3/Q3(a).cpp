e31b5e22a1f400061128e2e526602bd045fcfcca
ParulRana2907 <parulrana2407@gmail.com>
2025-02-08 13:16:01 +0530

    Q3(a).cpp
    .cpp

--- Assignment 1/Q3/Q3(a).cpp
+++ Assignment 1/Q3/Q3(a).cpp
@@ -1,1 +1,13 @@
+// accept a string from the user
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string input;
+    cout << "Please enter : ";
+    getline(cin, input);
+    cout << "You have entered: " << input << endl;
+    return 0;
+}
+
 
