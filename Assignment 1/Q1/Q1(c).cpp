9509101d9dd445768653ecdb04fc2fbf047dde47
ParulRana2907 <parulrana2407@gmail.com>
2025-02-08 13:05:29 +0530

    Q1(c).cpp
    .cpp

--- Assignment 1/Q1/Q1(c).cpp
+++ Assignment 1/Q1/Q1(c).cpp
@@ -1,1 +1,25 @@
+//If the number is prime check the next prime number greater than n
+#include <iostream>
+using namespace std;
+int main() {
+int n;
+cout << "Enter a number: ";
+cin >> n;
 
+int next = n + 1; 
+
+while (true) {  
+int i;
+        for (i = 2; i < next; i++) {
+ if (next % i == 0)
+            break; 
+        }
+if (i == next) {
+            cout << "Next prime number after " << n << " is: " << next << endl;
+            break;
+        }
+        next++;
+    }
+
+    return 0;
+}
